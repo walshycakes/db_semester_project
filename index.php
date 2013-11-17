244eda30257409a960dcee0c42d2b6b51c822323
walshycakes <bpw7xx@virginia.edu>
2013-11-17 18:19:41 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+   <!DOCTYPE html>
+    <html lang="en">
+    <?php 
+        $con=mysqli_connect("stardock.cs.virginia.edu","cs4750bpw7xx", "Q3[.kGfw", "cs4750bpw7xx");
+        if(mysqli_connect_errno($con)){
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        ?>
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+            <title></title>
+            <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>    
+    <div class="container">
+
+    <h1>Bryan Walsh</h1>
+    <h2>DB Semester Project</h2>
+
+    <p>CS 4700 -- Databases</p>
+
+    </div>
+    </body>
+    </html>
