b0d59caf615a8d1d751a533c44783503845a627c
walshycakes <bpw7xx@virginia.edu>
2013-11-30 16:28:54 -0500

    aa

---
+++ explore_artist.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+            <title></title>
+            <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+            <?php require_once('preheader.php');?>
+	</head>
+
+	<body>
+		<a href="./"><< home &nbsp;&nbsp;</a>
+
+		<h1> Artist:  Genre: </h1>
+		<h2>Record Label<h2>
+		<h2>Band Members</h2>
+		<h2>Albums</h2>
+		<h2>Songs</h2>
+		<h2>Awards</h2>
+		<h2>Performances</h2>
+		<h2>Tours</h2>
+
+	</body>
+
+</html>
---
+++ random_playlist.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+            <title></title>
+            <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+            <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+            <?php require_once('preheader.php');?>
+	</head>
+
+	<body>
+		<a href="./"><< home &nbsp;&nbsp;</a>
+
+		<h1><u>(Genre) Playlist</u></h1>
+		<ul>
+			<li>
+				Item 1
+			</li>
+		</ul>
+
+	</body>
+
+</html>
